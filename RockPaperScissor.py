f17410cbb9fc897db0a0d96a76718c7aad81b887
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2024-10-24 23:56:09 +0530

    Update RockPaperScissor.py

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,11 +1,17 @@
 import cv2
 import cvzone
 from cvzone.HandTrackingModule import HandDetector
+import time
+
 cap=cv2.VideoCapture(0)
 cap.set(3,640) # setting the third id which is width to 640
 cap.set(4,480) # setting the height to 480
 
+timer=0
 detector = HandDetector(maxHands=1)
+StateResult=False
+StartGame=False
+
 while True:
     imgBG = cv2.imread("RPS\\Resources\\Resources\\BG.png")
     success , img = cap.read()
@@ -14,10 +20,16 @@ while True:
     
     hands , img = detector.findHands(imgscaled)
     
-    if hands:
-        hand = hands[0]
-        fingers = detector.fingersUp(hand)
-        print(fingers)
+    if StartGame:
+        
+        if StateResult is False:
+            timer = time.time() - initialTime
+            cv2.putText(imgBG, str(int(timer)), (605, 435), cv2.FONT_HERSHEY_PLAIN, 6, (255, 0, 255), 4)
+        
+        if hands:
+            hand = hands[0]
+            fingers = detector.fingersUp(hand)
+            print(fingers)
         
     imgBG[234:654,795:1195]=imgscaled
     
@@ -26,4 +38,8 @@ while True:
     cv2.imshow("BG" , imgBG)
     #cv2.imshow("Scaled Image" , imgscaled)
     
-    cv2.waitKey(1)
+    key=cv2.waitKey(1)
+    
+    if (key==ord('s')):
+        StartGame=True
+        initialTime=time.time()
